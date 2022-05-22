95130750bcfca8af72ec176a13317d6087e07e65
Hirochon <heacet43@gmail.com>
2022-05-22 15:48:37 +0900

    add: comment to interface

--- sharp_pencil/sharp_pencil.go
+++ sharp_pencil/sharp_pencil.go
@@ -1,8 +1,9 @@
+// Package sharp_pencil はシャー芯を管理するパッケージです。
 package sharp_pencil
 
 type ISharpPencil interface {
-	WriteLetters() (outPencilNum, error)
-	PushButton() (outPencilNum, error)
-	RefillLead()
-	PencilNum() outPencilNum
+	WriteLetters() (outPencilNum, error) // 外に出ているシャー芯を1つ消費する
+	PushButton() (outPencilNum, error)   // 中のシャー芯を1つ消費して、外のシャー芯を1つ追加する
+	RefillLead()                         // 中にシャー芯を5つ追加する
+	PencilNum() outPencilNum             // 外に出ているシャー芯の数を返す
 }
