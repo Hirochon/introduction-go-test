73d0bcbcf612515fe2af28f207cd61872b46568c
Kanta Ozaki <forwkan@gmail.com>
2022-06-15 15:38:27 +0900

    interface lecture from pencilNum to RefillLead function

--- main.go
+++ main.go
@@ -3,15 +3,14 @@ package main
 import (
 	"fmt"
 
-	"github.com/Hirochon/introduction-go-test/error_handling"
 	"github.com/Hirochon/introduction-go-test/sharp_pencil"
 )
 
 func main() {
-	println(sum(2, 2))
-	if err := error_handling.RequestValidation(500); err != nil {
-		println(err.Error())
-	}
+	// println(sum(2, 2))
+	// if err := error_handling.RequestValidation(500); err != nil {
+	// 	println(err.Error())
+	// }
 	sharpenKatikati()
 }
 
@@ -21,57 +20,56 @@ func sum(a, b int) int {
 
 func sharpenKatikati() {
 	sharpPencil := sharp_pencil.InitializeSharpPencil()
-	fmt.Println(sharpPencil.PencilNum())
-	//fmt.Println("出ているシャー芯", sharpPencil.PencilNum())
+	fmt.Println("出ているシャー芯", sharpPencil.PencilNum())
 
-	//func() {
-	//	num, err := sharpPencil.PushButton()
-	//	if err != nil {
-	//		fmt.Println(err.Error())
-	//	}
-	//	fmt.Println("シャーペンカチカチ: 出ているシャー芯の長さ", num)
-	//}()
-	//
-	//func() {
-	//	num, err := sharpPencil.PushButton()
-	//	if err != nil {
-	//		fmt.Println(err.Error())
-	//	}
-	//	fmt.Println("シャーペンカチカチ: 出ているシャー芯の長さ", num)
-	//}()
-	//
-	//func() {
-	//	num, err := sharpPencil.WriteLetters()
-	//	if err != nil {
-	//		fmt.Println(err.Error())
-	//	}
-	//	fmt.Println("文字を書き書き: 出ているシャー芯の長さ", num)
-	//}()
-	//
-	//sharpPencil.RefillLead()
-	//fmt.Println("シャー芯を補充！")
-	//
-	//func() {
-	//	num, err := sharpPencil.PushButton()
-	//	if err != nil {
-	//		fmt.Println(err.Error())
-	//	}
-	//	fmt.Println("シャーペンカチカチ: 出ているシャー芯の長さ", num)
-	//}()
-	//
-	//func() {
-	//	num, err := sharpPencil.PushButton()
-	//	if err != nil {
-	//		fmt.Println(err.Error())
-	//	}
-	//	fmt.Println("シャーペンカチカチ: 出ているシャー芯の長さ", num)
-	//}()
-	//
-	//func() {
-	//	num, err := sharpPencil.WriteLetters()
-	//	if err != nil {
-	//		fmt.Println(err.Error())
-	//	}
-	//	fmt.Println("文字を書き書き: 出ているシャー芯の長さ", num)
-	//}()
+	func() {
+		num, err := sharpPencil.PushButton()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println("シャーペンカチカチ: 出ているシャー芯の長さ", num)
+	}()
+
+	func() {
+		num, err := sharpPencil.PushButton()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println("シャーペンカチカチ: 出ているシャー芯の長さ", num)
+	}()
+
+	func() {
+		num, err := sharpPencil.WriteLetters()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println("文字を書き書き: 出ているシャー芯の長さ", num)
+	}()
+
+	sharpPencil.RefillLead()
+	fmt.Println("シャー芯を補充！")
+
+	func() {
+		num, err := sharpPencil.PushButton()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println("シャーペンカチカチ: 出ているシャー芯の長さ", num)
+	}()
+
+	func() {
+		num, err := sharpPencil.PushButton()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println("シャーペンカチカチ: 出ているシャー芯の長さ", num)
+	}()
+
+	func() {
+		num, err := sharpPencil.WriteLetters()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println("文字を書き書き: 出ているシャー芯の長さ", num)
+	}()
 }
